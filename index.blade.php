d80396dc77dec70a7a303a5fa86b1d5308a461d4
MaryKargar <92923078+MaryKargar@users.noreply.github.com>
2022-04-17 18:33:12 +0200

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,18 @@
+@extends("layouts.admin")
+
+@section('content')
+
+    <div class="panel">
+
+        <div class="header">
+            Benutzer verwaltung
+        </div>
+
+
+        <div class="panel_content">
+            Benutzer Liste
+        </div>
+
+    </div>
+
+@endsection
